96ae221ddf5b88b44c6e9e44a37b652ca950ca59
Cameron Reikes <cameronreikes@gmail.com>
2023-07-19 18:16:59 -0700

    Bad looking fluid thing...

--- main.js
+++ main.js
@@ -1,114 +1,270 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { SelectiveBloomEffect, EffectComposer, EffectPass, RenderPass, BlendFunction, KernelSize } from "postprocessing";
+// WebGL - Render To Texture
+// from https://webglfundamentals.org/webgl/webgl-render-to-texture.html
 
-const size = 512.0;
-const size_width = size;
-const size_height = size;
+ "use strict";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, size_width / size_height, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer({
-    powerPreference: "high-performance",
-    antialias: false,
-    stencil: false,
-    depth: false
-});
-renderer.setSize( size_width, size_height );
-renderer.useLegacyLights = false;
-//renderer.setClearColor(new THREE.Color(0xf1f1f1))
-renderer.setClearAlpha(0.0);
-document.body.appendChild( renderer.domElement );
-
-const composer = new EffectComposer(renderer);
-composer.addPass(new RenderPass(scene, camera));
-const bloom = new SelectiveBloomEffect(scene, camera, {
-    blendFunction: BlendFunction.ADD,
-    mipmapBlur: true,
-    luminanceThreshold: 0.4,
-    luminanceSmoothing: .3,
-    intensity: 2.0,
-    kernelSize: KernelSize.SMALL,
-  });
-bloom.luminancePass.enabled = false;
-bloom.inverted = true;
-composer.addPass(new EffectPass(camera, bloom));
-
-
-const uniforms = {
-  u_time: { value: 0.0 },
-}
-const vShader = `
-  varying vec2 v_uv;
-  varying vec3 v_pos;
-  varying vec3 v_normal;
-
-  uniform float u_time;
-
-  #include <normal_pars_vertex>
-
-
-  void main() {
-     v_uv = uv;
-     vec3 moved_pos = position;
-     moved_pos.x += sin(u_time*5.0 + position.y*2.0)*0.05;
-     moved_pos.y += cos(u_time*5.0 + position.y*3.0)*0.03;
-     v_pos = moved_pos;
-     v_normal = normal;
-     gl_Position = projectionMatrix * modelViewMatrix * vec4(moved_pos, 1.0);
+function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  var canvas = document.getElementById("canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
   }
-`;
 
-const fShader = `
-  varying vec2 v_uv;
-  varying vec3 v_pos;
-  varying vec3 v_normal;
-  uniform float u_time;
+  gl.enable(gl.BLEND);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+  //gl.blendFunc(0,0);
 
-void main() {
-  vec3 normal = normalize(cross(dFdx(v_pos.xyz), dFdy(v_pos.xyz)));
-  float brightness = pow(length(v_pos), 5.0)/2.0;
-  //gl_FragColor = vec4(vec3(0.4,0.4,1)*brightness, brightness).rgba;
-  float fresnel = clamp(pow(length(v_uv - vec2(0.5, 0.5)), 2.0)*5.0, 0.0, 1.0);
-  //fresnel = 1.0 - fresnel;
-  gl_FragColor = vec4(vec3(0.3,0.3,1.0)*brightness*fresnel,(fresnel/2.0));
-  /*
-  if(length(v_uv - center) < 0.3) {
-    gl_FragColor = vec4(1.0);
-  } else {
-    gl_FragColor = vec4(0,0,0,1.0);
+  // setup GLSL program
+  var program = webglUtils.createProgramFromScripts(gl, ["3d-vertex-shader", "3d-fragment-shader"]);
+
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+  var texcoordLocation = gl.getAttribLocation(program, "a_texcoord");
+
+  // lookup uniforms
+  var textureLocation = gl.getUniformLocation(program, "u_texture");
+
+  var timeLocation = gl.getUniformLocation(program, "time");
+
+  // Create a buffer for positions
+  var positionBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Put the positions in the buffer
+  setGeometry(gl);
+
+  // provide texture coordinates for the rectangle.
+  var texcoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  // Set Texcoords.
+  setTexcoords(gl);
+
+  // Create a texture.
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  {
+    // fill texture with 3x2 pixels
+    const level = 0;
+    const internalFormat = gl.LUMINANCE;
+    const width = 3;
+    const height = 2;
+    const border = 0;
+    const format = gl.LUMINANCE;
+    const type = gl.UNSIGNED_BYTE;
+    const data = new Uint8Array([
+      128,  64, 128,
+        0, 192,   0,
+    ]);
+    const alignment = 1;
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, alignment);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border,
+                  format, type, data);
+
+    // set the filtering so we don't need mips and it's not filtered
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   }
-  */
+
+  // Create a texture to render to
+  const targetTextureWidth = 256;
+  const targetTextureHeight = 256;
+  const targetTexture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, targetTexture);
+
+  {
+    // define size and format of level 0
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const border = 0;
+    const format = gl.RGBA;
+    const type = gl.UNSIGNED_BYTE;
+    const data = null;
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  targetTextureWidth, targetTextureHeight, border,
+                  format, type, data);
+
+    // set the filtering so we don't need mips
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  }
+
+  // Create and bind the framebuffer
+  const fb = gl.createFramebuffer();
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+
+  // attach the texture as the first color attachment
+  const attachmentPoint = gl.COLOR_ATTACHMENT0;
+  const level = 0;
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, targetTexture, level);
+
+  function degToRad(d) {
+    return d * Math.PI / 180;
+  }
+
+  var fieldOfViewRadians = degToRad(60);
+  var modelXRotationRadians = degToRad(0);
+  var modelYRotationRadians = degToRad(0);
+
+  // Get the starting time.
+  var then = 0;
+
+  requestAnimationFrame(drawScene);
+
+  function drawCube(aspect, time) {
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 3;          // 3 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        positionLocation, size, type, normalize, stride, offset);
+
+    // Turn on the teccord attribute
+    gl.enableVertexAttribArray(texcoordLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+        texcoordLocation, size, type, normalize, stride, offset);
+
+    // Compute the projection matrix
+    var projectionMatrix =
+        m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
+
+    var cameraPosition = [0, 0, 2];
+    var up = [0, 1, 0];
+    var target = [0, 0, 0];
+
+    // Compute the camera's matrix using look at.
+    var cameraMatrix = m4.lookAt(cameraPosition, target, up);
+
+    // Make a view matrix from the camera matrix.
+    var viewMatrix = m4.inverse(cameraMatrix);
+
+    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
+
+    // Tell the shader to use texture unit 0 for u_texture
+    gl.uniform1i(textureLocation, 0);
+
+    gl.uniform1f(timeLocation, time);
+
+    // Draw the geometry.
+    gl.drawArrays(gl.TRIANGLES, 0, 6 * 6);
+  }
+
+  // Draw the scene.
+  function drawScene(time) {
+    // convert to seconds
+    time *= 0.001;
+    // Subtract the previous time from the current time
+    var deltaTime = time - then;
+    // Remember the current time for the next frame.
+    then = time;
+
+    // Animate the rotation
+    modelYRotationRadians += -0.7 * deltaTime;
+    modelXRotationRadians += -0.4 * deltaTime;
+
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    //gl.enable(gl.CULL_FACE);
+    //gl.enable(gl.DEPTH_TEST);
+
+    /*
+    {
+      // render to our targetTexture by binding the framebuffer
+      gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+
+      // render cube with our 3x2 texture
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+
+      // Tell WebGL how to convert from clip space to pixels
+      gl.viewport(0, 0, targetTextureWidth, targetTextureHeight);
+
+      // Clear the canvas AND the depth buffer.
+      gl.clearColor(0, 0, 0, 1);   // clear to blue
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      const aspect = targetTextureWidth / targetTextureHeight;
+      drawCube(aspect, time);
+    }
+    */
+
+    {
+      // render to the canvas
+      gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+      // render the cube with the texture we just rendered to
+      gl.bindTexture(gl.TEXTURE_2D, targetTexture);
+
+      // Tell WebGL how to convert from clip space to pixels
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+      // Clear the canvas AND the depth buffer.
+      gl.clearColor(0, 0, 0, 0);   // clear to white
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      //gl.clear(0);
+
+      const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+      drawCube(aspect, time);
+    }
+
+    requestAnimationFrame(drawScene);
+  }
 }
-`;
-const material = new THREE.ShaderMaterial({
-  vertexShader: vShader,
-  fragmentShader: fShader,
-  uniforms
-});
 
-const loader = new GLTFLoader();
-
-loader.load('sphere.glb', function ( gltf ) {
-  gltf.scene.traverse((o) => {
-    if (o.isMesh) o.material = material;
-  });
-
-	scene.add( gltf.scene );
-}, undefined, function ( error ) {
-	console.error( error );
-});
-
-camera.position.z = 5;
-
-const clock = new THREE.Clock();
-
-function animate() {
-	requestAnimationFrame( animate );
-
-  uniforms.u_time.value = clock.getElapsedTime();
-  composer.render();
-	//renderer.render( scene, camera );
+// Fill the buffer with the values that define a cube.
+function setGeometry(gl) {
+  var positions = new Float32Array(
+    [
+    -1.0, -1.0,  -1.0,
+    -1.0,  1.0,  -1.0,
+     1.0, -1.0,  -1.0,
+    -1.0,  1.0,  -1.0,
+     1.0,  1.0,  -1.0,
+     1.0, -1.0,  -1.0,
+    ]);
+  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
 }
-animate();
+
+// Fill the buffer with texture coordinates the cube.
+function setTexcoords(gl) {
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(
+        [
+          0, 0,
+          0, 1,
+          1, 0,
+          0, 1,
+          1, 1,
+          1, 0,
+
+      ]),
+      gl.STATIC_DRAW);
+}
+
+main();
