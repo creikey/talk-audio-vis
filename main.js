e852b7befcf5da6ec9d97ae3dd58c4bb4e137c2d
Cameron Reikes <cameronreikes@gmail.com>
2023-07-22 18:23:10 -0700

    Minimalistic

--- main.js
+++ main.js
@@ -41,35 +41,6 @@ function main() {
   // Set Texcoords.
   setTexcoords(gl);
 
-  // Create a texture.
-  var texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
-  {
-    // fill texture with 3x2 pixels
-    const level = 0;
-    const internalFormat = gl.LUMINANCE;
-    const width = 3;
-    const height = 2;
-    const border = 0;
-    const format = gl.LUMINANCE;
-    const type = gl.UNSIGNED_BYTE;
-    const data = new Uint8Array([
-      128,  64, 128,
-        0, 192,   0,
-    ]);
-    const alignment = 1;
-    gl.pixelStorei(gl.UNPACK_ALIGNMENT, alignment);
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border,
-                  format, type, data);
-
-    // set the filtering so we don't need mips and it's not filtered
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  }
-
   // Create a texture to render to
   const targetTextureWidth = 256;
   const targetTextureHeight = 256;
